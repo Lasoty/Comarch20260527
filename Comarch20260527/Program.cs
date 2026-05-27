6b5ec4badbc1048fe8c3070acee14262316f97c1
Leszek Lewandowski <Leszek.Lewandowski@dnv.com>
2026-05-27 13:00:35 +0200

    Thread safe rozwiązanie

--- Comarch20260527/Program.cs
+++ Comarch20260527/Program.cs
@@ -30,21 +30,57 @@ internal static class Program
 
     private static void RunWithLock()
     {
+        var counter = 0;
+        object sync = new();
 
+        Parallel.For(0, Iterations, _ =>
+        {
+            lock (sync)
+            {
+                counter++;
+            }
+        });
+
+        Console.WriteLine($"Counter with lock:             {counter}");
     }
 
     private static void RunWithInterlocked()
     {
-
+        var counter = 0;
+        Parallel.For(0, Iterations, _ =>
+        {
+            Interlocked.Increment(ref counter);
+        });
+        Console.WriteLine($"Counter with Interlock:             {counter}");
     }
 
     private static async Task RunWithSemaphoreSlimAsync()
     {
-
+        var counter = 0;
+        using var semaphore = new SemaphoreSlim(1, 1);
+        var tasks = Enumerable.Range(0, Iterations).Select(async _ =>
+        {
+            await semaphore.WaitAsync();
+            try
+            {
+                counter++;
+            }
+            finally
+            {
+                semaphore.Release();
+            }
+        });
+        await Task.WhenAll(tasks);
+        Console.WriteLine($"Counter with semaphoreSlim:             {counter}");
     }
 
     private static void RunWithConcurrentDictionary()
     {
-
+        var dictionary = new ConcurrentDictionary<string, int>();
+        Parallel.For(0, Iterations, _ =>
+        {
+            dictionary.AddOrUpdate("counter", 1, (_, oldvalue) => oldvalue + 1);
+        });
+        Console.WriteLine($"Counter in dictionary:             {dictionary["counter"]}");
     }
 }
